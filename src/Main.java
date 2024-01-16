299e2a2c50615b00e8dc5fc76bd30482fae6da96
Hardwoker <hardwoker2017@yandex.ru>
2024-01-16 23:17:32 +0500

    Homework done!

--- src/Main.java
+++ src/Main.java
@@ -313,5 +313,100 @@ public class Main {
         }else if (three > one && three > two){
             System.out.println(three);
         }
+        System.out.println("Домашняя работа №5");
+        System.out.println("Задание 1");
+        byte clientOS = 1;
+        switch (clientOS){
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            default:
+                System.out.println("Ошибка");
+        }
+        System.out.println("Задание 2");
+        byte clientIOS = 1;
+        byte clientAndroid = 0;
+        short yearOfIssueClient1 = 2012;
+        byte myClient1 = 1;
+        short yearOfIssueClient2 = 2022;
+        byte myClient2 = 0;
+        short limitingYearOfRelease = 2015;
+        if (myClient1 == clientIOS && yearOfIssueClient1 < limitingYearOfRelease){
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        }else if (myClient1 == clientIOS && yearOfIssueClient1 > limitingYearOfRelease){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }else if (myClient1 == clientAndroid && yearOfIssueClient1 < limitingYearOfRelease){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }else if (myClient1 == clientAndroid && yearOfIssueClient1 > limitingYearOfRelease){
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        if (myClient2 == clientIOS && yearOfIssueClient2 < limitingYearOfRelease){
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        }else if (myClient2 == clientIOS && yearOfIssueClient2 > limitingYearOfRelease){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }else if (myClient2 == clientAndroid && yearOfIssueClient2 < limitingYearOfRelease){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }else if (myClient2 == clientAndroid && yearOfIssueClient2 > limitingYearOfRelease){
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        System.out.println("Задание 3");
+        short year = 2021;
+        short startYear = 1584;
+        if (year > startYear && year % 4 == 0 && year % 100 != 0 || year > startYear && year % 400 == 0) {
+            System.out.println(year + " год является високосным.");
+        } else {
+            System.out.println(year + " год не является високосным.");
+        }
+        System.out.println("Задание 4");
+        byte deliveryDistance = 95;
+        byte day = 1;
+        byte distance1 = 20;
+        byte distance2 = 60;
+        byte distance3 = 100;
+        if (deliveryDistance <= distance1){
+            System.out.println("Потребуется дней: " + day);
+        } else if (deliveryDistance >= distance1 && deliveryDistance <= distance2){
+            System.out.println("Потребуется дней: " + day + day);
+        } else if (deliveryDistance >= distance2 && deliveryDistance <= distance3){
+            System.out.println("Потребуется дней: " + (day + day + day));
+        } else if (deliveryDistance > distance3){
+            System.out.println("Доставки нет");
+        }
+        System.out.println("Задание 5");
+        byte monthNumber = 7;
+        switch (monthNumber){
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Ошибка");
+        }
+
+
+
+
+
+
+
     }
 }
